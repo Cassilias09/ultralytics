6e5ff519b4628af8857ac2a6ca3519d2b6364dda
Cassiano Sabino <cass.sabino@gmail.com>
2023-09-07 18:32:49 -0300

    Update track.py

--- ultralytics/trackers/track.py
+++ ultralytics/trackers/track.py
@@ -54,7 +54,7 @@ def on_predict_postprocess_end(predictor):
         if True in ball_idx: # Há detecções da bola
             # Armazena as bounding boxes das detecções da bola
             ball_detections = predictor.results[i][ball_idx]
-            ball_bboxes = ball_detections.boxes.data.cpu().numpy()
+            ball_bboxes = ball_detections.cpu().boxes.data.numpy()
             # Altera o array das bboxes para ser compatível com o array do tracking
             ball_bboxes = np.insert(ball_bboxes, 4, -1, axis=1) # xyxy, track_id = -1, conf, cls
             ball_bboxes = np.append(ball_bboxes, np.full((ball_bboxes.shape[0], 1), -1), axis=1) # xyxy, track_id = -1, conf, cls, idx = -1
